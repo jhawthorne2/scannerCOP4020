33b5cdc4562f035d1c319df42ccc458fa809b0ad
ArgoFromLargo <lukekledzik@gmail.com>
2017-03-11 18:03:34 -0600

    Trying to clean up functionality in order to progress

--- Input.java
+++ Input.java
@@ -1,11 +1,39 @@
-public class Input{
-    public char getCharacter(){
+import java.io.*;
 
+public class Input{
+    public FileInputStream in;
+    public boolean eof;
+
+    public Input(String inputFileName){
+        try{
+            this.in = new FileInputStream(inputFileName);
+        }
+        catch(Exception ex){
+            System.out.println("Error creating input file stream");
+        }
+
+        this.eof = false;
+    }
+
+    public char getCharacter(){
+        int character;
+
+        try{
+            character = in.read();
+        }
+        catch(Exception ex){
+            System.out.println("Error reading character from file stream");
+        }
+
+        if(character == -1){
+            eof = true;
+            return '';
+        }
     }
 
     // Return true if failed
     public boolean failed(){
-
+        return eof;
     }
     public void pushback(){
 
--- Scanner.java
+++ Scanner.java
@@ -12,6 +12,17 @@ public class Scanner{
         try{
             in = new FileInputStream(args[0]);
             out = new FileOutputStream(args[1]);
+        }
+        catch(Exception ex){
+            System.out.println("Error creating file streams");
+        }
+
+        scanner(in, out);
+
+        /*
+        try{
+            in = new FileInputStream(args[0]);
+            out = new FileOutputStream(args[1]);
             int character;
             while ((character = in.read()) != -1){
                 out.write(character);
@@ -31,10 +42,11 @@ public class Scanner{
         catch(Exception ex){
             System.out.println("Error closing FileStreams");
         }
+        */
     }
 
 
-    public void scanner(){
+    public void scanner(FileInputStream in, FileOutputStream out){
         Lexeme lexeme;
         lexeme = lex();
         while(lexeme.getType() != END){
@@ -44,28 +56,28 @@ public class Scanner{
         }
     }
 
-    public Lexeme lex(){
+    public Lexeme lex(FileInputStream in){
         char ch;
         skipWhiteSpace();
-        ch = Input.getCharacter();
+        ch = Input.getCharacter(in);
         if(Input.failed())
-            return new Lexeme(END);
+            return new Lexeme(Type.END);
         switch(ch){
             // single character tokens
             case '(':
-                return new Lexeme(OPAREN);
+                return new Lexeme(Type.OPAREN);
             case ')':
-                return new Lexeme(CPAREN);
+                return new Lexeme(Type.CPAREN);
             case ',':
-                return new Lexeme(COMMA);
+                return new Lexeme(Type.COMMA);
             case '+':
-                return new Lexeme(PLUS);
+                return new Lexeme(Type.PLUS);
             case '*':
-                return new Lexeme(TIMES);
+                return new Lexeme(Type.TIMES);
             case '-':
-                return new Lexeme(MINUS);
+                return new Lexeme(Type.MINUS);
             case '/':
-                return new Lexeme(DIVIDES);
+                return new Lexeme(Type.DIVIDES);
             default:
                 // multi-character tokens
                 // (only numbers, variables/keywords, and strings)
@@ -83,7 +95,7 @@ public class Scanner{
                     return lexString();
                 }
         }
-        return new Lexeme(BAD_CHARACTER, ch);
+        return new Lexeme(Type.BAD_CHARACTER, ch);
     }
 
     public static void confirmArgs(String[] args){
