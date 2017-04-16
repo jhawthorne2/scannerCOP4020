048e359db67ca6e3648fe0e22775efb5dc4cf011
ArgoFromLargo <lukekledzik@gmail.com>
2017-04-15 20:50:07 -0500

    added some method signatures

---
+++ Env/Environment.java
@@ -0,0 +1,33 @@
+public class Environment {
+
+    // Creates a new lexeme with the type, left child, and right child spec.
+    public Lexeme join(Type type, Lexeme left, Lexeme right) {
+
+    }
+
+    // Returns the left child of the lexeme
+    public Lexeme head(Lexeme lexeme) {
+
+    }
+
+    // Sets the left child of the lexeme
+    public void setHead(Lexeme lexeme) {
+
+    }
+
+    // Returns the right child of the lexeme
+    public Lexeme tail(Lexeme lexeme) {
+
+    }
+
+    // Sets the right child of the lexeme
+    public void setTail(Lexeme lexeme) {
+
+    }
+
+    // Returns the left child of the right child of the lexeme
+    public Lexeme htail(Lexeme lexeme) {
+
+    }
+
+}
